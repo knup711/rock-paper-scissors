bfa067fe6fc68362d76e1a1cbd5c934949a9134d
knup711 <127697480+knup711@users.noreply.github.com>
2023-09-25 16:34:16 +0200

    fix the prompt, so that the first l is always a capital letter

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,4 +1,4 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['Rock', 'Paper', 'Scissors'];
 
 let ComputerChoice = getComputerChoice();
 
@@ -18,7 +18,7 @@ function getComputerChoice () {
 function getPlayersChoice () {
      let Playersprompt = prompt('Please choose rock, paper or scissors');
 
-     let PlayersChoice = Playersprompt.toLowerCase();
+     let PlayersChoice = Playersprompt.charAt(0).toUpperCase() + Playersprompt.slice(1).toLowerCase();
 
      return PlayersChoice;
 }
@@ -28,17 +28,17 @@ function getPlayersChoice () {
 
 if (PlayersChoice == ComputerChoice) {
     alert("Undecided, " + PlayersChoice + " cant beat " + ComputerChoice + ", because it's the same.");
-} else if (PlayersChoice == "rock" && ComputerChoice == "scissors") {
+} else if (PlayersChoice == "Rock" && ComputerChoice == "Scissors") {
     alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
-} else if (PlayersChoice == "rock" && ComputerChoice == "paper") {
+} else if (PlayersChoice == "Rock" && ComputerChoice == "Paper") {
     alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + ".");
-} else if (PlayersChoice == "paper" && ComputerChoice == "rock") {
+} else if (PlayersChoice == "Paper" && ComputerChoice == "Rock") {
     alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
-} else if (PlayersChoice == "paper" && ComputerChoice == "scissors") {
+} else if (PlayersChoice == "Paper" && ComputerChoice == "Scissors") {
     alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + "."); 
-} else if (PlayersChoice == "scissors" && ComputerChoice == "rock") {
+} else if (PlayersChoice == "Scissors" && ComputerChoice == "Rock") {
     alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + ".");
-} else if (PlayersChoice == "scissors" && ComputerChoice == "paper") {
+} else if (PlayersChoice == "Scissors" && ComputerChoice == "Paper") {
     alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
 }
     
