0298e7f550e160e8b4e38a69d2fda458b7d18245
knup711 <127697480+knup711@users.noreply.github.com>
2023-09-25 15:35:31 +0200

    add the function for the computerchoice

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rock-paper-scissors
+
+Hello!
+
+This is my rock, paper, scissors project from the odin project.
+In the first version, I will try to write a rock, paper, scissors game in
+javascript for the console.
+To solve this problem, I will write a pseudocode to understand all steps, which
+are necessary.
+
+Greetings
+Phil
--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,17 @@
-console.log("Hello Wolrd!");
+const choices = ['rock', 'paper', 'scissors'];
+
+const ComputerChoice = getComputerChoice();
+
+//create the Computerchoice
+
+console.log(choices);
+
+function getComputerChoice () {
+    let ComputerChoice = choices[Math.floor(Math.random() * 3)];
+
+    return ComputerChoice;
+
+
+}
+
+console.log (ComputerChoice);
