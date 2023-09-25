6c73fa0b772fa57a2d76b56cc975650086908674
knup711 <127697480+knup711@users.noreply.github.com>
2023-09-25 16:30:47 +0200

    add players choice and compare the choices to find the winner

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,17 +1,50 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-const ComputerChoice = getComputerChoice();
+let ComputerChoice = getComputerChoice();
 
-//create the Computerchoice
+let PlayersChoice = getPlayersChoice();
 
-console.log(choices);
+//create the Computer choice
 
 function getComputerChoice () {
     let ComputerChoice = choices[Math.floor(Math.random() * 3)];
 
     return ComputerChoice;
 
-
 }
 
-console.log (ComputerChoice);
+//create the Players choice
+
+function getPlayersChoice () {
+     let Playersprompt = prompt('Please choose rock, paper or scissors');
+
+     let PlayersChoice = Playersprompt.toLowerCase();
+
+     return PlayersChoice;
+}
+
+
+//compare Players choice with the computer choice and alert the winner
+
+if (PlayersChoice == ComputerChoice) {
+    alert("Undecided, " + PlayersChoice + " cant beat " + ComputerChoice + ", because it's the same.");
+} else if (PlayersChoice == "rock" && ComputerChoice == "scissors") {
+    alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
+} else if (PlayersChoice == "rock" && ComputerChoice == "paper") {
+    alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + ".");
+} else if (PlayersChoice == "paper" && ComputerChoice == "rock") {
+    alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
+} else if (PlayersChoice == "paper" && ComputerChoice == "scissors") {
+    alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + "."); 
+} else if (PlayersChoice == "scissors" && ComputerChoice == "rock") {
+    alert("Sorry you lost, " + ComputerChoice + " beats " + PlayersChoice + ".");
+} else if (PlayersChoice == "scissors" && ComputerChoice == "paper") {
+    alert("Congratulation, you won this round! " + PlayersChoice +" beats " + ComputerChoice + ".");
+}
+    
+
+
+
+
+
+
