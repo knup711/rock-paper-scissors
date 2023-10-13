c954cf7a6a78f52937a80d9f75cb4bebcd1e8f74
knup711 <127697480+knup711@users.noreply.github.com>
2023-10-13 14:11:13 +0200

    fix playRound, so a playround starts after click event

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,3 +1,15 @@
+// get necessary DOM nodes
+
+const resultbox = document.querySelector(".resultbox");
+const plrWin = document.querySelector(".plrWin");
+const playround = document.querySelector(".playround");
+const cmptrWin = document.querySelector(".cmptrWin");
+const remis = document.querySelector(".remis");
+const div = document.querySelector("div");
+
+let content = document.createElement ("p");
+let resulttext = document.createElement ("p");
+
 const choices = ['Rock', 'Paper', 'Scissors'];
 
 let playerWin = 0;
@@ -5,18 +17,14 @@ let computerWin = 0;
 let roundCount = 0;
 let remi = 0;
 let playersChoice;
+let playersSelect;
 let computerChoice = getComputerChoice();
 
-let resultbox = document.querySelector(".resultbox");
-let content = document.createElement ("p");
-let resulttext = document.createElement ("p");
-let plrWin = document.querySelector(".plrWin");
+// show playrounds and score
+
 plrWin.textContent = playerWin;
-let playround = document.querySelector(".playround");
 playround.textContent = roundCount;
-let cmptrWin = document.querySelector(".cmptrWin");
 cmptrWin.textContent = computerWin;
-let remis = document.querySelector(".remis");
 remis.textContent = remi;
 
 
@@ -28,18 +36,21 @@ function getComputerChoice () {
 
     return computerChoice;
 
-}
+};
 
-//create the Players choice
+//create the Players choice and start a playRound
 
 function getPlayersChoice() {
-let div = document.querySelector("div");
-
-div.addEventListener("click", () => {
-    let playersChoice = event.target.id;
-    playRound(playersChoice, computerChoice);
-});
-    return;
+    div.addEventListener("click", () => {
+       playersChoice = event.target.id;
+       console.log(playersChoice);
+        computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        playRound(playersChoice, computerChoice);
+        if (playerWin == 5 || computerWin == 5) {
+            result();
+        }
+    })
 };
 
 
@@ -66,7 +77,8 @@ function playRound (playersChoice, computerChoice) {
 
     let win = "Congratulation, you won this round! " + playersChoice +" beats " + computerChoice + ".";
     let lost = "Sorry you lost, " + computerChoice + " beats " + playersChoice + ".";
-    let undecided = "Undecided, " + playersChoice + " cant beat " + computerChoice + ", because it's the same.";
+    let undecided = "Undecided, " + playersChoice + " can't beat " + computerChoice + ", because it's the same.";
+
    if (playersChoice == computerChoice) {
     roundCount++;
     remi++;
@@ -74,52 +86,42 @@ function playRound (playersChoice, computerChoice) {
     resultbox.appendChild(content);
     playround.textContent = roundCount;
     remis.textContent = remi;
-} else if (playersChoice == "Rock" && computerChoice == "Scissors") {
-    roundCount++
-    playerWin++;
-    content.textContent = win;
-    resultbox.appendChild(content);
-    playround.textContent = roundCount;
-    plrWin.textContent = playerWin;
-} else if (playersChoice == "Rock" && computerChoice == "Paper") {
-    roundCount++;
-    computerWin++;
-    content.textContent = lost;
-    resultbox.appendChild(content);
-    playround.textContent = roundCount;
-    cmptrWin.textContent = computerWin;
-} else if (playersChoice == "Paper" && computerChoice == "Rock") {
+} else if ((playersChoice == "Rock" && computerChoice == "Scissors")    ||
+            (playersChoice == "Paper" && computerChoice == "Rock")      ||
+            (playersChoice == "Scissors" && computerChoice == "Paper")) {
     roundCount++;
     playerWin++;
     content.textContent = win;
     resultbox.appendChild(content);
     playround.textContent = roundCount;
     plrWin.textContent = playerWin;
-} else if (playersChoice == "Paper" && computerChoice == "Scissors") {
+} else if ((playersChoice == "Rock" && computerChoice == "Paper")        ||
+            (playersChoice == "Paper" && computerChoice == "Scissors")   ||
+            (playersChoice == "Scissors" && computerChoice == "Rock")) {
     roundCount++;
     computerWin++;
     content.textContent = lost;
     resultbox.appendChild(content);
     playround.textContent = roundCount;
     cmptrWin.textContent = computerWin;
-} else if (playersChoice == "Scissors" && computerChoice == "Rock") {
-    roundCount++;
-    computerWin++;
-    content.textContent = lost;
-    resultbox.appendChild(content);
-    playround.textContent = roundCount;
-    cmptrWin.textContent = computerWin;
-} else if (playersChoice == "Scissors" && computerChoice == "Paper") {
-    roundCount++;
-    playerWin++;
-    content.textContent = win;
-    resultbox.appendChild(content);
-    playround.textContent = roundCount;
-    plrWin.textContent = playerWin;
 }
-    return;
 };
 
-for (i = 0; i < 5; i++) {
+function game() {
     getPlayersChoice();
+    if (playerWin == 5 || computerWin == 5) {
+        clearCounters();
+       return result();
+    }
 };
+
+
+function clearCounters() {
+    let playerWin = 0;
+    let computerWin = 0;
+    let roundCount = 0;
+    let remi = 0;
+
+};
+
+game();
